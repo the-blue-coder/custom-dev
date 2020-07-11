0796ca8f5d89bc2e12e3ccad7f9364ff657ed36e
madainsight <49811195+madainsight@users.noreply.github.com>
2020-07-11 13:23:48 +0300

    Update Assets.php

--- app/hooks/Assets.php
+++ app/hooks/Assets.php
@@ -6,37 +6,44 @@ class Assets
 {
     public function __construct()
     {
-        #Load CSS
-        add_action('admin_enqueue_scripts', [$this, 'loadCSS']);
-        add_action('wp_enqueue_scripts', [$this, 'loadCSS']);
-
-        #Load JS
-        add_action('admin_enqueue_scripts', [$this, 'loadJS']);
-        add_action('wp_enqueue_scripts', [$this, 'loadJS']);
+        $this->loadCSS();
+        $this->loadJS();
     }
 
 
 
     public function loadCSS()
     {
-        wp_enqueue_style(
-            'custom-dev', 
-            CUSTOM_DEV_PLUGIN_DIST_URL . 'css/custom-styles.min.css', 
-            [],
-            rand(1, 10000)
-        );
+        add_action('admin_enqueue_scripts', 'doCSSLoading');
+        add_action('wp_enqueue_scripts', 'doCSSLoading');
+        
+        function doCSSLoading()
+        {
+            wp_enqueue_style(
+                'custom-dev', 
+                CUSTOM_DEV_PLUGIN_DIST_URL . 'css/custom-styles.min.css', 
+                [],
+                rand(1, 10000)
+            );
+        }
     }
 
 
 
     public function loadJS()
     {
-        wp_enqueue_script(
-            'custom-dev-js', 
-            CUSTOM_DEV_PLUGIN_DIST_URL . 'js/custom-scripts.min.js', 
-            [], 
-            rand(1, 10000)
-        );
+        add_action('admin_enqueue_scripts', 'doJSLoading');
+        add_action('wp_enqueue_scripts', 'doJSLoading');
+        
+        function doJSLoading()
+        {
+            wp_enqueue_script(
+                'custom-dev-js', 
+                CUSTOM_DEV_PLUGIN_DIST_URL . 'js/custom-scripts.min.js', 
+                [], 
+                rand(1, 10000)
+            );
+        }
     }
 }
 
