0a196317f4a8d71f1b02eb718f24c08932749320
Jean R <jrakotoarison@bocasay.com>
2020-07-30 10:12:35 +0300

    2.1.1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin name: Custom Dev
  * Description: Plugin Framework for WordPress Development
  * Author: Jean R.
- * Version: 2.1.0
+ * Version: 2.1.1
  */
 
 require 'vendor/autoload.php';
