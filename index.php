7a3f6393bee473f2c03c61dcc8f0dcefb02c1c8e
the-blue-coder <49811195+the-blue-coder@users.noreply.github.com>
2020-07-31 16:44:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  * Plugin name: Custom Dev
  * Description: Plugin Framework for WordPress Development
  * Author: Jean R.
- * Version: 2.2.0
+ * Version: 2.2.1
  */
 
 require 'vendor/autoload.php';
